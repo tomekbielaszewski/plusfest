fec7ad01a0c62faa149460f4f84efd5736dfe3a9
Tomek <tomekbielaszewski@gmail.com>
2017-09-18 23:38:38 +0200

    Support for https on wykop.pl

--- tampermonkey/plusfest.user.js
+++ tampermonkey/plusfest.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name       plusfest
-// @namespace  http://wykop.pl/
+// @namespace  https://wykop.pl/
 // @version    1.0
 // @description  #plusafera
-// @match      http://www.wykop.pl/*
+// @match      https://www.wykop.pl/*
 // @copyright  2017, @Grizwold
 // @updateURL   https://github.com/tomekbielaszewski/plusfest/raw/master/tampermonkey/plusfest.user.js
 // @installURL  https://github.com/tomekbielaszewski/plusfest/raw/master/tampermonkey/plusfest.user.js
