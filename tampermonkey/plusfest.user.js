72cae3855cdb57323bc3378906ccfa9ab0c267ff
Tomek <tomekbielaszewski@gmail.com>
2017-09-18 23:38:55 +0200

    Version increment

--- tampermonkey/plusfest.user.js
+++ tampermonkey/plusfest.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       plusfest
 // @namespace  https://wykop.pl/
-// @version    1.0
+// @version    1.1
 // @description  #plusafera
 // @match      https://www.wykop.pl/*
 // @copyright  2017, @Grizwold
